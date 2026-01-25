f79dee13457b2dd12c37623069fe30e41a4b4881
alanlasco <61526254+alanlasco@users.noreply.github.com>
2026-01-25 17:37:29 -0300

    setting off

---
+++ src/components/dashboard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/dashboard.css";
+export const dashboard = () => {
+  return (
+    <div className="dashboard">
+      <div className="section">Sección 1</div>
+      <div className="section">Sección 2</div>
+      <div className="section">Sección 3</div>
+      <div className="section">Sección 4</div>
+    </div>
+  );
+};
